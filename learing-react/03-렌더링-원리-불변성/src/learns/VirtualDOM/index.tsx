5f1706a6844499aa3418b4af4aa1a15baaab02d6
yamoo9 <yamoo9@naver.com>
2026-03-11 08:11:08 +0900

    refactor. 학습 자료 이동
    - Accordion
    - TabMenu

--- learing-react/03-렌더링-원리-불변성/src/learns/VirtualDOM/index.tsx
+++ learing-react/03-렌더링-원리-불변성/src/learns/VirtualDOM/index.tsx
@@ -1,5 +1,28 @@
 import S from './style.module.css'
 
+/**
+ * 리액트 렌더 파이프라인
+ * - Trigger → Render → Commit
+ * 
+ * 브라우저 페인팅(렌더링)
+ * - Painting
+ * 
+ * 리액트 상태(State)
+ * - 스냅샵(Snaphost)과 유사
+ * - 렌더링 시, 불변(Immutable)
+ * - 배칭(Batching): 업데이트 큐(Queue)
+ * 
+ * 리액트 제어(Controlled)
+ * - 리액트 제어
+ * - 상태 기반 Input 제어
+ * - 선언적 프로그래밍
+ * 
+ * 리액트 비제어(Uncontrolled)
+ * - 브라우저 제어
+ * - 웹 표준 기반 Input 제어
+ * - 명령적 프로그래밍
+ */
+
 export default function VirtualDOM() {
   return (
     <section className={S.wrapper}>
