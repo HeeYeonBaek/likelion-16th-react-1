ce83c44ead18a01b0dbec697cccf111433070d73
yamoo9 <yamoo9@naver.com>
2026-03-11 08:46:32 +0900

    refactor. 학습 자료

--- learing-react/03-렌더링-원리-불변성/src/app/index.tsx
+++ learing-react/03-렌더링-원리-불변성/src/app/index.tsx
@@ -1,10 +1,10 @@
-import { VirtualDOM } from '@/learns'
+import { RenderPipeline } from '@/learns'
 import S from './style.module.css'
 
 export default function App() { 
   return (
     <div className={S.container}>
-      <VirtualDOM />
+      <RenderPipeline />
     </div>
   )
 }
